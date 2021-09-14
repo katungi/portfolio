81d5896ebfe71159c0397529683c54db81836247
Daniel Dennis <dankatdennis@gmail.com>
2021-09-14 21:35:34 +0300

    - added command modal styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Router from 'next/router';
 import nprogress from 'nprogress';
 import debounce from 'lodash.debounce';
+import '../styles/globals.css';
 
 const start = debounce(nprogress.start, 500);
 Router.events.on('routeChangeStart', start);
@@ -14,7 +15,6 @@ Router.events.on('routeChangeError', () => {
   start.cancel();
   nprogress.done();
 });
-
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Page from '../components/page';
 import Link from 'next/link';
 
@@ -9,28 +8,27 @@ export default function Home() {
         <h1>Daniel Katungi Dennis</h1>
 
         <p>
-          software developer and designer, Tech enthusiast, practicing
-          minimalist, and{' '}
+          software developer , Tech enthusiast, practicing minimalist.{' '}
           <Link underline href='/music'>
-            music lover
+            Also a music lover
           </Link>{' '}
           in search of flow.{' '}
           <Link underline href='/blog'>
-            Writing
+            I Write alot
           </Link>{' '}
           about javascript mostly
         </p>
 
         <p>
-          Making magic at{' '}
+          Making magic at ,{' '}
           <Link underline href='https://castify.ai' external>
             Castify
-          </Link>{' '}
-          Writing stuff at
+          </Link>
+          Writing stuff at{' '}
           <Link underline href='https://section.io' external>
             Section
           </Link>{' '}
-          and making databases easy to use at
+          and making databases easy to use at{' '}
           <Link underline href='https://www.prisma.io/' external>
             Prisma
           </Link>{' '}
