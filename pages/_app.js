1e27a926a05f8f8849098ce19f88d2275a617256
Daniel Dennis <dankatdennis@gmail.com>
2021-09-14 19:59:37 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import React from 'react';
+import Router from 'next/router';
+import '../styles/globals.css';
+
+const start = debounce(nprogress.start, 500);
+Router.events.on('routeChangeStart', start);
+Router.events.on('routeChangeComplete', () => {
+  start.cancel();
+  nprogress.done();
+  window.scrollTo(0, 0);
+});
+Router.events.on('routeChangeError', () => {
+  start.cancel();
+  nprogress.done();
+});
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
