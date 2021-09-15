a44c56f1db8ad35274428569d47f687e64bd516a
Daniel Dennis <dankatdennis@gmail.com>
2021-09-15 06:34:31 +0300

    - added contacts

---
+++ pages/contact.js
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <Page title='Contact' footer={false} description='Get in touch.'>
+      <article>
+        <p>Get in touch.</p>
+
+        <blockquote>
+          <a
+            href='mailto:dankatdennis@gmail.com?subject=Hello'
+            className='reset'
+          >
+            dankatdennis@gmail.com
+          </a>
+        </blockquote>
+      </article>
+    </Page>
+  );
+};
+
+export default Contact;
